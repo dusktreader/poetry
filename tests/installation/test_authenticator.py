8305e6eae9b4bf885cf4468535275ff371722993
Tucker Beck <tucker.beck@officeally.com>
2020-12-14 08:56:32 -0800

    Issue #3483 Fixed style issues with black

--- tests/installation/test_authenticator.py
+++ tests/installation/test_authenticator.py
@@ -211,10 +211,9 @@ def test_authenticator_uses_certs_from_config_if_not_provided(
         {
             "repositories": {"foo": {"url": "https://foo.bar/simple/"}},
             "http-basic": {"foo": {"username": "bar", "password": "baz"}},
-            "certificates": {"foo": {
-                "cert": "/path/to/cert",
-                "client-cert": "/path/to/client-cert",
-            }},
+            "certificates": {
+                "foo": {"cert": "/path/to/cert", "client-cert": "/path/to/client-cert"}
+            },
         }
     )
 
@@ -233,10 +232,9 @@ def test_authenticator_uses_provided_certs_instead_of_config_certs(
         {
             "repositories": {"foo": {"url": "https://foo.bar/simple/"}},
             "http-basic": {"foo": {"username": "bar", "password": "baz"}},
-            "certificates": {"foo": {
-                "cert": "/path/to/cert",
-                "client-cert": "/path/to/client-cert",
-            }},
+            "certificates": {
+                "foo": {"cert": "/path/to/cert", "client-cert": "/path/to/client-cert"}
+            },
         }
     )
 
